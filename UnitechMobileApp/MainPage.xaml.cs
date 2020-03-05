b393f7dbb742542144625d894782d47cb7f7404b
Tapochec <and_petr_00@mail.ru>
2020-03-05 21:38:27 +0300

    Дишмаская авторизация

--- UnitechMobileApp/MainPage.xaml.cs
+++ UnitechMobileApp/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -16,6 +19,33 @@ namespace UnitechMobileApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string login = LoginInput.Text;
+            string password = PasswordInput.Text;
+
+            string requestText = "https://ies.unitech-mo.ru/api?token=e78a4a9c0b16dd06b0ebc4748345a144&query=AUTH&" + 
+                $"login={login}&password={password}";
+
+            WebRequest request = WebRequest.Create(requestText);
+            request.Method = "POST";
+
+            WebResponse response = request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Debug.WriteLine(line);
+                    }
+                }
+            }
+            response.Close();
         }
     }
 }
