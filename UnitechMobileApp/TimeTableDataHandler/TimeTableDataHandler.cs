97ddd17a690ab470eaf81e071b1749da2f4e608f
Alexander <sanek41km@mail.ru>
2020-03-06 19:57:36 +0300

    парсер расписания из json

---
+++ UnitechMobileApp/TimeTableDataHandler/TimeTableDataHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TimeTable
+{
+    public class DataTimeTableData
+    {
+        public List<TimeTableDay> data;
+        public List<TimeTableDay> holydays;
+    }
+
+    public class TimeTableDay
+    {
+        public int daynum;
+        public int timenum; // номер пары
+        public int weeknum; // 1 - чётная, 2 - не чётнаяб 3 - по обеим
+        public string lparam;
+        public string note;
+    }
+
+
+    /// <summary>
+    /// Принимает на вход json и парсит его в объект. Пары, которые выпали на праздничный день, заменяются на праздник
+    /// </summary>
+    class TimeTableDataHandler
+    {
+        public static DataTimeTableData Data;
+
+        public static void JsonToTimeTableData(string rawData)
+        {
+            var a = JsonConvert.DeserializeObject<DataTimeTableData>(rawData);
+
+            //filtering data from holidays
+            foreach(var item in a.holydays)
+            {
+                for(int i = 0; i < a.data.Count; ++i)
+                {
+                    if(a.data[i].daynum == item.daynum)
+                    {
+                        a.data[i] = item;
+                    }
+                }
+            }
+            Data = a;
+        }
+    }
+}
