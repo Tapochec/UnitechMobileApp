f730b321aee6d74d46ca87b2774b0201db255290
AStr347 <anton3000str@mail.ru>
2020-03-10 23:47:05 +0300

    feat(Models): Academic - класс предмета; feat(ViewModels): добавил JournalViewModel

---
+++ UnitechMobileApp/Models/Academic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitechMobileApp.Models
+{
+    class Academic
+    {
+        public string name;
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ UnitechMobileApp/ViewModels/JournalViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text;
+using UnitechMobileApp.Models;
+using UnitechMobileApp.Views;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace UnitechMobileApp.ViewModels
+{
+    class JournalViewModel : INotifyPropertyChanged
+    {
+        public List<Academic> Academics { get; private set; }
+
+        private JournalPage journalPage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+
+
+        public JournalViewModel(JournalPage page) {
+            journalPage = page;
+            Academics = new List<Academic>() { new Academic() { name = "Математика" }, new Academic() { name = "Биология" }, 
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" },
+                new Academic() { name = "Химия" }, new Academic() { name = "Физика" }, new Academic() { name = "Физ-ра" }};
+            //PropertyChanged(this, new PropertyChangedEventArgs("Academics"));
+        }
+    }
+}
