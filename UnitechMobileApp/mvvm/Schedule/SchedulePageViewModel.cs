73e9f5db8b5cadeeb48cd0e6035b745d7138558c
petrenko <a.petrenko@ia-group.ru>
2020-03-13 15:22:06 +0300

    Добавил пустую модель представления для расписания

---
+++ UnitechMobileApp/mvvm/Schedule/SchedulePageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ClientApi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitechMobileApp.mvvm.Schedule
+{
+    class SchedulePageViewModel
+    {
+        public SchedulePageViewModel()
+        {
+
+        }
+
+        public void Load()
+        {
+            
+        }
+    }
+}
