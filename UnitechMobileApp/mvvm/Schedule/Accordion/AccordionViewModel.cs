e5eeb22e03b2032c31b85ed651eb7f0f1fcd41a0
petrenko <a.petrenko@ia-group.ru>
2020-03-20 09:51:06 +0300

    Добавил триггер включить/выключить для аккордиона, если на этот день нет пар

--- UnitechMobileApp/mvvm/Schedule/Accordion/AccordionViewModel.cs
+++ UnitechMobileApp/mvvm/Schedule/Accordion/AccordionViewModel.cs
@@ -10,11 +10,27 @@ namespace UnitechMobileApp.mvvm.Schedule.Accordion
 {
     public class AccordionViewModel : ViewModelBase
     {
+        private bool enabled;
+        public bool Enabled
+        {
+            get { return enabled; }
+            set { SetProperty(ref enabled, value); }
+        }
+
+
         public List<ScheduleDisplayLesson> lessons;
         public List<ScheduleDisplayLesson> Lessons
         {
             get { return lessons; }
-            set { SetProperty(ref lessons, value);  }
+            set
+            {
+                SetProperty(ref lessons, value);
+
+                if (lessons?.Count == 0)
+                    Enabled = false;
+                else
+                    Enabled = true;
+            }
         }
 
         public AccordionViewModel()
