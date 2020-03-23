f67ef5564503efe14061617269db8fb7457ef23b
AStr347 <anton3000str@mail.ru>
2020-03-23 23:08:51 +0300

    feat(academ): page viewmodel model для предмета

---
+++ UnitechMobileApp/mvvm/Academ/AcademPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitechMobileApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UnitechMobileApp.mvvm.Academ
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcademPage : ContentPage
+    {
+        AcademViewModel vm;
+
+        public AcademPage(IAcademic academic)
+        {
+            InitializeComponent();
+            vm = new AcademViewModel(this, academic);
+
+            BindingContext = vm;
+        }
+    }
+}
